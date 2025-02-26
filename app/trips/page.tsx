5bbdded3d35eb3e9f7e85dcee480c465af21a27d
Vinicius Vicentini <viniciusvicentini@live.com>
2025-02-26 14:53:03 -0300

    fix build

--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -1,17 +1,15 @@
-import { getTravel, getTrips } from '@/services/requests'
+import { getTrips } from '@/services/requests'
 import dynamic from 'next/dynamic'
 import { TripMap } from './_components/map'
 
 const TripSidebar = dynamic(() => import('./_components/sidebar'))
 
 const TripsPage = async () => {
-  const [trips, travel] = await Promise.all([getTrips(), getTravel()])
-
-  const currentLocation = travel.at(0)
+  const trips = await getTrips()
 
   return (
     <>
-      <TripMap data={trips} currentLocation={currentLocation} />
+      <TripMap data={trips} />
 
       <TripSidebar data={trips} />
     </>
