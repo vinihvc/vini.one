7d8249609f149934f595b944bc5d8f60f17d580a
Vinicius Vicentini <viniciusvicentini@live.com>
2025-02-28 02:35:45 -0300

    fix blog post

--- app/(landing)/blog/(post)/layout.tsx
+++ app/(landing)/blog/(post)/layout.tsx
@@ -1,6 +1,6 @@
 const BlogPostLayout = async ({ children }: React.PropsWithChildren) => {
   return (
-    <div className="prose-invert container text-muted-foreground prose-a:no-underline selection:bg-rose-500">
+    <div className="prose-invert prose container text-muted-foreground prose-a:no-underline selection:bg-rose-500">
       {children}
     </div>
   )
