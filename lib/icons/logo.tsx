4e83e215a175f908b721e326b682f25660f005d1
Vinicius Vicentini <viniciusvicentini@live.com>
2025-03-08 04:13:51 -0300

    updating logo

--- lib/icons/logo.tsx
+++ lib/icons/logo.tsx
@@ -1,11 +1,6 @@
 import { createIcon } from '../../components/ui/create-icon'
 
 export const Logo = createIcon({
-  viewBox: '0 0 22 26',
-  path: (
-    <path
-      d="M5.81075 0.447998L12.6908 17.408L10.0028 16.864L16.2428 0.447998H21.7468L10.9948 25.088L0.24275 0.447998H5.81075Z"
-      fill="currentColor"
-    />
-  ),
+  viewBox: '0 0 232 222',
+  d: 'M100.578 221.641L0.5 0.15625H72.0312L116 120.906L159.969 0.15625H231.5L131.422 221.641H100.578Z',
 })
