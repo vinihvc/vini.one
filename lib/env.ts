da297708c30950b0bac64f0fe3489c4ab684105b
Vinicius Vicentini <viniciusvicentini@live.com>
2025-08-04 14:48:37 -0300

    adding app

--- lib/env.ts
+++ lib/env.ts
@@ -1,26 +1,26 @@
-import { createEnv } from '@t3-oss/env-nextjs'
-import { z } from 'zod'
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
 
 export const env = createEnv({
-  server: {
-    CLOUDINARY_CLOUD_NAME: z.string().min(1),
-    CLOUDINARY_API_KEY: z.string().min(1),
-    CLOUDINARY_API_SECRET: z.string().min(1),
-  },
-  client: {
-    NEXT_PUBLIC_UMAMI_WEBSITE_ID: z.string().min(1),
-    NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN: z.string().min(1),
-    NEXT_PUBLIC_MAPBOX_USER_ID: z.string().min(1),
-    NEXT_PUBLIC_MAPBOX_STYLE_ID: z.string().min(1),
-  },
-  runtimeEnv: {
-    NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN:
-      process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN,
-    NEXT_PUBLIC_UMAMI_WEBSITE_ID: process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID,
-    NEXT_PUBLIC_MAPBOX_USER_ID: process.env.NEXT_PUBLIC_MAPBOX_USER_ID,
-    NEXT_PUBLIC_MAPBOX_STYLE_ID: process.env.NEXT_PUBLIC_MAPBOX_STYLE_ID,
-    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
-    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
-    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
-  },
-})
+	server: {
+		CLOUDINARY_CLOUD_NAME: z.string().min(1),
+		CLOUDINARY_API_KEY: z.string().min(1),
+		CLOUDINARY_API_SECRET: z.string().min(1),
+	},
+	client: {
+		NEXT_PUBLIC_UMAMI_WEBSITE_ID: z.string().min(1),
+		NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN: z.string().min(1),
+		NEXT_PUBLIC_MAPBOX_USER_ID: z.string().min(1),
+		NEXT_PUBLIC_MAPBOX_STYLE_ID: z.string().min(1),
+	},
+	runtimeEnv: {
+		NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN:
+			process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN,
+		NEXT_PUBLIC_UMAMI_WEBSITE_ID: process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID,
+		NEXT_PUBLIC_MAPBOX_USER_ID: process.env.NEXT_PUBLIC_MAPBOX_USER_ID,
+		NEXT_PUBLIC_MAPBOX_STYLE_ID: process.env.NEXT_PUBLIC_MAPBOX_STYLE_ID,
+		CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+		CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+		CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+	},
+});
--- lib/icons/index.tsx
+++ lib/icons/index.tsx
@@ -1,29 +1,31 @@
-import { SiGithub, SiInstagram, SiX } from '@icons-pack/react-simple-icons'
+import { SiGithub, SiInstagram, SiX } from "@icons-pack/react-simple-icons";
 import {
-  Album,
-  CalendarSearch,
-  Clock,
-  Crown,
-  Linkedin,
-  Omega,
-  Salad,
-  TrendingDown,
-} from 'lucide-react'
-import { Eletrohits } from './eletrohits'
-import { Logo } from './logo'
+	Album,
+	CalendarSearch,
+	Clock,
+	CloudDownload,
+	Crown,
+	Linkedin,
+	Omega,
+	Salad,
+	TrendingDown,
+} from "lucide-react";
+import { Eletrohits } from "./eletrohits";
+import { Logo } from "./logo";
 
 export const Icons = {
-  github: SiGithub,
-  x: SiX,
-  linkedin: Linkedin,
-  instagram: SiInstagram,
-  eletrohits: Eletrohits,
-  os: Omega,
-  books: Album,
-  clock: Clock,
-  idleval: Crown,
-  optimizer: TrendingDown,
-  date: CalendarSearch,
-  fit: Salad,
-  logo: Logo,
-} as const
+	github: SiGithub,
+	x: SiX,
+	linkedin: Linkedin,
+	instagram: SiInstagram,
+	eletrohits: Eletrohits,
+	os: Omega,
+	apphub: CloudDownload,
+	books: Album,
+	clock: Clock,
+	idleval: Crown,
+	optimizer: TrendingDown,
+	date: CalendarSearch,
+	fit: Salad,
+	logo: Logo,
+} as const;
