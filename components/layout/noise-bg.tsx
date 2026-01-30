23c1b43b0da2d632482b5aea971bce468b065efb
Vinicius Vicentini <viniciusvicentini@live.com>
2026-01-30 12:46:30 -0600

    updating gradient bg to be tailwind calass

--- components/layout/noise-bg.tsx
+++ components/layout/noise-bg.tsx
@@ -1,15 +1,17 @@
-import { BlurImage } from "../ui/blur-image";
+import { cn } from "@/lib/cn";
 
 const NoiseBg = () => {
   return (
     <>
-      <BlurImage
-        alt="Custom Background"
-        className="absolute inset-x-0 top-0 -z-1 mx-auto opacity-30"
-        height={670}
-        priority
-        src="/images/gradient-bg.png"
-        width={1000}
+      <div
+        aria-hidden
+        className={cn(
+          "absolute inset-0 -top-40 -z-1",
+          "opacity-50",
+          "pointer-events-none",
+          "mask-[radial-gradient(circle_at_50%_0%,white_0%,transparent_80%)]",
+          "bg-[linear-gradient(to_right,rgba(20_184_166/0.25)_0%,rgba(59_130_246/0.22)_17%,rgba(139_92_246/0.2)_33%,rgba(239_68_68/0.18)_50%,rgba(249_115_22/0.18)_67%,rgba(234_179_8/0.15)_83%,transparent_100%)]"
+        )}
       />
 
       <svg
