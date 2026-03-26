acf5cc1db0cf9c9b6a927f9dcd5526bf26a0be47
Vinicius Vicentini <viniciusvicentini@live.com>
2026-03-25 21:56:53 -0600

    try fix build

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,13 @@ import mdx from "fumadocs-mdx/vite";
 import { nitro } from "nitro/vite";
 import vinext from "vinext";
 import { defineConfig } from "vite";
+// biome-ignore lint/performance/noNamespaceImport: it's ok
 import * as MdxConfig from "./source.config";
 
 export default defineConfig({
+  nitro: {
+    preset: "vercel",
+  },
   // Avoid Vite from bundling React contexts and duplicated context conflicts (fumadocs template).
   optimizeDeps: {
     exclude: ["fumadocs-ui", "fumadocs-core"],
